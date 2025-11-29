c9b17c9645d978658cd5019809fbf83f71f01726
CaptainN <32107345+The-Captain-N@users.noreply.github.com>
2025-11-29 17:34:17 +0200

    header icon

--- src/components/layout/Header.vue
+++ src/components/layout/Header.vue
@@ -1,7 +1,7 @@
 <template>
 	<header>
 		<div class="title clipped-x-large-forward">
-			<img class="logo" src="/faction-logos/atlas_black.svg" />
+			<img class="logo_header" src="/faction-logos/atlas_black.svg" />
 			<div class="title-container">
 				<div id="title-first-line" class="title-row">
 					<span id="title-header">{{ header.headerTitle }}</span>
