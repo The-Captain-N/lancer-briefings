ac170b2d65c03b9a225b0a6d09c5cbdc69c31b84
CaptainN <32107345+The-Captain-N@users.noreply.github.com>
2026-02-13 22:52:31 +0200

    legion update

--- src/components/Pilot.vue
+++ src/components/Pilot.vue
@@ -208,7 +208,7 @@ export default {
 		return [...ktbData.bonds]
 		},
 		frames() {
-		return [...lancerData.frames, ...nrfawData.frames]
+		return [...lancerData.frames, ...nrfawData.frames, ...legionnaireData.frames]
 		},
 		mechManufacturerIcon() {
 		if (this.activeMech.manufacturer)
