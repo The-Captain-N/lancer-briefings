d29e1f5764354046040d1b92cc9b6f2d74c27ef0
CaptainN <32107345+The-Captain-N@users.noreply.github.com>
2026-02-13 23:25:51 +0200

    small fix

--- src/components/Pilot.vue
+++ src/components/Pilot.vue
@@ -208,7 +208,7 @@ export default {
 		return [...ktbData.bonds]
 		},
 		frames() {
-		return [...lancerData.frames, ...nrfawData.frames, ...winterscarData.frames, ...LancerEnhancedCombatAltFramesData.frame]
+		return [...lancerData.frames, ...nrfawData.frames, ...winterscarData.frames, ...LancerEnhancedCombatAltFramesData.frames]
 		},
 		mechManufacturerIcon() {
 		if (this.activeMech.manufacturer)
